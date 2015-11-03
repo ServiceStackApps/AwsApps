9349db3276f700998baeba165a267ca76cb95f5f
Demis Bellot <demis.bellot@gmail.com>
2015-11-02 21:01:51 -0800

    Add initial awsauth app

--- src/AwsApps/AppHost.cs
+++ src/AwsApps/AppHost.cs
@@ -3,8 +3,12 @@ using Amazon;
 using Amazon.S3;
 using Funq;
 using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Authentication.OAuth2;
+using ServiceStack.Authentication.OpenId;
 using ServiceStack.Aws.DynamoDb;
 using ServiceStack.Aws.S3;
+using ServiceStack.Caching;
 using ServiceStack.IO;
 using ServiceStack.Razor;
 using ServiceStack.Text;
@@ -20,6 +24,8 @@ namespace AwsApps
         {
             JsConfig.EmitCamelCaseNames = true;
 
+            Plugins.Add(new RazorFormat());
+
             //Comment out 2 lines below to change to use local FileSystem instead of S3
             var s3Client = new AmazonS3Client(AwsConfig.AwsAccessKey, AwsConfig.AwsSecretKey, RegionEndpoint.USEast1);
             VirtualFiles = new S3VirtualPathProvider(s3Client, AwsConfig.S3BucketName, this);
@@ -29,14 +35,40 @@ namespace AwsApps
             db.RegisterTable<Todo>();
             db.InitSchema();
 
-            Plugins.Add(new RazorFormat());
+            //AWS Auth
+            container.Register<ICacheClient>(new DynamoDbCacheClient(db));
+            container.Register<IAuthRepository>(new DynamoDbAuthRepository(db));
+            container.Resolve<IAuthRepository>().InitSchema();
+            Plugins.Add(CreateAuthFeature());
         }
 
-        public override List<IVirtualPathProvider> GetVirtualPathProviders()
+        public override List<IVirtualPathProvider> GetVirtualFileSources()
         {
-            var pathProviders = base.GetVirtualPathProviders();
-            pathProviders.Add(VirtualFiles);
-            return pathProviders;
+            var fileSources = base.GetVirtualFileSources();
+            fileSources.Add(VirtualFiles);
+            return fileSources;
+        }
+
+        public AuthFeature CreateAuthFeature()
+        {
+            return new AuthFeature(() => new AuthUserSession(),
+                new IAuthProvider[]
+                {
+                    new CredentialsAuthProvider(),              //HTML Form post of UserName/Password credentials
+                    new BasicAuthProvider(),                    //Sign-in with HTTP Basic Auth
+                    new DigestAuthProvider(AppSettings),        //Sign-in with HTTP Digest Auth
+                    new TwitterAuthProvider(AppSettings),       //Sign-in with Twitter
+                    new FacebookAuthProvider(AppSettings),      //Sign-in with Facebook
+                    new YahooOpenIdOAuthProvider(AppSettings),  //Sign-in with Yahoo OpenId
+                    new OpenIdOAuthProvider(AppSettings),       //Sign-in with Custom OpenId
+                    new GoogleOAuth2Provider(AppSettings),      //Sign-in with Google OAuth2 Provider
+                    new LinkedInOAuth2Provider(AppSettings),    //Sign-in with LinkedIn OAuth2 Provider
+                    new GithubAuthProvider(AppSettings),        //Sign-in with GitHub OAuth Provider
+                })
+            {
+                HtmlRedirect = "/awsauth/",
+                IncludeRegistrationService = true,
+            };
         }
     }
 }
---
+++ src/AwsApps/awsauth/AwsAuthService.cs
@@ -0,0 +1,69 @@
+﻿using ServiceStack;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using ServiceStack.Auth;
+using ServiceStack.Aws.DynamoDb;
+
+namespace AwsAuth
+{
+    [Route("/awsauth/session")]
+    public class GetSession : IReturn<GetSessionResponse> {}
+
+    public class GetSessionResponse
+    {
+        public AuthUserSession Result { get; set; }
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+
+    [Route("/awsauth/reset")]
+    public class Reset { }
+
+    public class AwsAuthService : Service
+    {
+        public object Any(GetSession request)
+        {
+            return new GetSessionResponse {
+                Result = SessionAs<AuthUserSession>(),
+            };
+        }
+
+        public object Any(Reset request)
+        {
+            ResetUsers((DynamoDbAuthRepository)TryResolve<IAuthRepository>());
+            return "OK";
+        }
+
+        public static void ResetUsers(DynamoDbAuthRepository authRepo)
+        {
+            authRepo.DeleteUserAuth(1);
+            authRepo.DeleteUserAuth(2);
+
+            CreateUser(authRepo, 1, "test", "test", new List<string> { "TheRole" }, new List<string> { "ThePermission" });
+            CreateUser(authRepo, 2, "test2", "test");
+        }
+
+        private static void CreateUser(IUserAuthRepository authRepo,
+            int id, string username, string password, List<string> roles = null, List<string> permissions = null)
+        {
+            string hash;
+            string salt;
+            new SaltedHash().GetHashAndSaltString(password, out hash, out salt);
+
+            var userAuth = authRepo.CreateUserAuth(new UserAuth
+            {
+                Id = id,
+                DisplayName = username + " DisplayName",
+                Email = username + "@gmail.com",
+                UserName = username,
+                FirstName = "First " + username,
+                LastName = "Last " + username,
+                PasswordHash = hash,
+                Salt = salt,
+                Roles = roles,
+                Permissions = permissions
+            }, password);
+
+            authRepo.AssignRoles(userAuth, roles, permissions);
+        }
+    }
+}
